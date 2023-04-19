8ad9c2b5107fe6da0bc88cd384f4151f3f0f63b4
kaitoben <3312ben@gmail.com>
2023-04-19 17:53:53 +0900

    バリデーションの設定2

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,24 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+
   validates :nickname, presence: true
-  validates :family_name, presence: true
-  validates :first_name, presence: true
-  validates :family_name_kana, presence: true
-  validates :first_name_kana, presence: true
+
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'に全角文字を使用してください' } do
+  validates :family_name
+  validates :first_name
+  end
+
+  with_options presence: true, format: { with: //\A[ァ-ヶー－]+\z/, message: 'に全角カタカナを使用してください' } do 
+  validates :family_name_kana
+  validates :first_name_kana
+  end
+
   validates :date_of_birth, presence: true
 
-  
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates :password, format: { with: VALID_PASSWORD_REGEX }
+
+
+
 end
