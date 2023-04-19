562fe6b9cfe68aa276dca66bda65fe5dbf6fc732
kaitoben <3312ben@gmail.com>
2023-04-19 19:13:44 +0900

    ログインの有無による表示変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,15 +12,18 @@ class User < ApplicationRecord
   validates :first_name
   end
 
-  with_options presence: true, format: { with: //\A[ァ-ヶー－]+\z/, message: 'に全角カタカナを使用してください' } do 
+  with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: 'に全角カタカナを使用してください' } do 
   validates :family_name_kana
   validates :first_name_kana
   end
 
-  validates :date_of_birth, presence: true
-
+  
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   validates :password, format: { with: VALID_PASSWORD_REGEX }
+
+
+
+  validates :date_of_birth, presence: true
 
 
 
