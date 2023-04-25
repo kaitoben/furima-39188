8d46f95c876c0fa34c1a86ce3707f0414894b12e
kaitoben <3312ben@gmail.com>
2023-04-25 15:57:13 +0900

    商品情報編集の変更1

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
 
- before_action :move_to_session, only: [:new]  
+ before_action :move_to_session, only: [:new, :edit]  
+ before_action :move_to_index, only: [:edit]
 
 def index
   @items = Item.all.order("created_at DESC")
@@ -23,6 +24,25 @@ def create
   end
 end
 
+
+def edit
+  @item = Item.find(params[:id])
+end
+
+
+def update
+  @item = Item.find(params[:id])
+  
+  if @item.update(item_params)
+    redirect_to item_path
+  else
+    render :edit
+  end
+end
+
+
+
+
 def show
   @item = Item.find(params[:id])
 end
@@ -33,11 +53,24 @@ def item_params
   params.require(:item).permit(:item_name, :explanation, :category_id, :status_id, :shipping_charge_id, :sender_id, :day_to_ship_id, :price, :image).merge(user_id: current_user.id)
 end
 
+
+
+
 def move_to_session
   unless user_signed_in? 
    redirect_to new_user_session_path
   end
- end
+end
+
+
+
+def move_to_index
+  @item = Item.find(params[:id])
+  unless  current_user.id == @item.user.id
+    redirect_to action: :index
+  end
+end
+
 
 
 end
