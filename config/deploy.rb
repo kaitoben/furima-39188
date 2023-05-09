3098b45c9101b8a5bfc684e51c63d6b6ec19ffcc
kaitoben <3312ben@gmail.com>
2023-05-09 20:32:27 +0900

    自動デプロイ7

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,7 +7,7 @@ set :application, 'furima-39188'
 # どのリポジトリからアプリをpullするかを指定する
 set :repo_url,  'git@github.com:kaitoben/furima-39188.git'
 
-set :branch, 'main'
+
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
